82167e6d748747ef59471c3f7ee4b4b316f9676c
yewon0325 <147733678+yewon0325@users.noreply.github.com>
2025-07-12 13:18:16 +0900

    홈탭 업데이트

--- HomeTap/home.py
+++ HomeTap/home.py
@@ -1,17 +1,40 @@
+from flask import Flask, request, jsonify
 import firebase_admin
 from firebase_admin import credentials, firestore
+from datetime import datetime
 
-cred = credentials.Certificate("HomeTap\serviceAccountKey.json")
+cred = credentials.Certificate("HomeTap/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
 
-# users 컬렉션에 user1 문서 추가
-doc_ref = db.collection("users").document("user1")
-doc_ref.set({
-    "name": "홍길동",
-    "email": "hong@example.com",
-    "point": 100
-})
+app = Flask(__name__)
 
-print("데이터가 성공적으로 추가되었습니다.")
+@app.route("/")
+def hello():
+    return "백엔드 서버가 정상 작동 중입니다."
+
+#  인증 정보 저장 API
+@app.route("/submit_certification", methods=["POST"])
+def submit_certification():
+    data = request.json  # 앱에서 보낸 JSON 데이터
+
+    uid = data["uid"]
+    mission = data["mission"]
+    photo_url = data["photo_url"]
+
+    today = datetime.now().strftime("%Y%m%d")
+    doc_id = f"{uid}_{today}"
+
+    # 인증 정보 Firestore에 저장
+    db.collection("certifications").document(doc_id).set({
+        "user_id": uid,
+        "mission": mission,
+        "date": today,
+        "photo_url": photo_url
+    })
+
+    return jsonify({"message": "인증 저장 완료!"})
+
+# Flask 실행
+if __name__ == "__main__":
+    app.run(debug=True)
